7e49399fba0b7a276ac06c79b7ad54d1811a61a4
Володя <118829074+Riminis@users.noreply.github.com>
2024-09-28 23:24:16 +0300

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -8,15 +8,27 @@ from .main import people_in_building
 
 app = Flask(__name__)
 
+def load_json_data(file_path):
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
+
+json_file_path = 'data/data-62743-2024-09-03.json'
+json_data = load_json_data(json_file_path)
+
 @app.route('/public', methods=['GET'])
-def handle_post_1():
+def handler():
     response = {
         "message": "Alive"
     }
-
     return jsonify(response), 200
+
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    return jsonify(json_data), 200
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=4000)
-
-
