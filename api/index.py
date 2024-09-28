a6b4082846c36ae261f94bbebae25bacebe0ab7e
Володя <118829074+Riminis@users.noreply.github.com>
2024-09-28 23:29:32 +0300

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -2,19 +2,17 @@ import json
 import pandas as pd
 from geopy.distance import geodesic
 import psycopg2
+import os
 from flask import Flask, request, jsonify
-
-from .main import people_in_building
 
 app = Flask(__name__)
 
 def load_json_data(file_path):
-    with open(file_path, 'r') as json_file:
+    with open(file_path, 'r', encoding='utf-8') as json_file:
         data = json.load(json_file)
     return data
 
-
-json_file_path = 'data/data-62743-2024-09-03.json'
+json_file_path = os.path.join(os.path.dirname(__file__), '../public/data-62743-2024-09-03.json')
 json_data = load_json_data(json_file_path)
 
 @app.route('/public', methods=['GET'])
@@ -24,11 +22,10 @@ def handler():
     }
     return jsonify(response), 200
 
-
 @app.route('/data', methods=['GET'])
 def get_data():
     return jsonify(json_data), 200
 
 
-if __name__ == '__main__':
-    app.run(host='localhost', port=4000)
+def handler(event, context):
+    return app(event, context)
